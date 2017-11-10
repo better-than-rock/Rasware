934758b4e4e90fc1b42e61df8fd406ce4f58e07e
Kenny Tang <kennyftang@gmail.com>
2017-11-10 17:44:21 -0600

    Working on debugigng

--- Robotathon17/Main.c
+++ Robotathon17/Main.c
@@ -3,6 +3,7 @@
 #include <RASLib/inc/time.h> 
 #include <RASLib/inc/adc.h>
 #include <RASLib/inc/sonar.h> 
+#include <RASLib/inc/linesensor.h> 
 #include <RASLib/inc/motor.h>
 #include <math.h>
 #include "Switch.h"
@@ -26,9 +27,11 @@ void SetSpeed(tMotor* motor, float power) {
 int main(void) {
     tMotor *left = InitializeServoMotor(PIN_B0, false);
     tMotor *right = InitializeServoMotor(PIN_B3, true);
-    tADC *disLeft = InitializeADC(PIN_E5);
-    tADC *disFront = InitializeADC(PIN_B4);
-    // tADC *disRight = InitializeADC(PIN_E2);
+    tADC *disLeft = InitializeADC(PIN_E4);
+    tADC *disRight = InitializeADC(PIN_E5);
+    SetPin(PIN_A5, true);
+    tLineSensor *line = InitializeGPIOLineSensor(PIN_E0, PIN_E3, PIN_E2, PIN_E1, PIN_D3, PIN_D2, PIN_D1, PIN_D0);
+    float lv[8]; 
     // Sweet spot is the desired distance from the wall to keep
     float sweetSpot = 0.3;
     float avgTriDelta = ADCRead(disLeft);
@@ -39,52 +42,35 @@ int main(void) {
     // float errorL = ADCRead(disLeft) - maxDist;
     // float errorR = ADCRead(disRight) - maxDist;
     // tBoolean lastPressed = false;
+    
+    SetSpeed(left, 0.0);
+    SetSpeed(right, 0.0);
 
     // Initialization code can go here
-    CallEvery(blink, 0, 0.5);
+    CallEvery(blink, 0, 0.1);
     // Runtime code can go here
-    Printf("Hello World!\n");
+    Printf("Initializing the death of you\n");
     int printCount = 0;
-    // These two bools are used to finish a turn
-    tBoolean insideTurn = false;
-    tBoolean findWall = false;
     while (1) {
-        float travelDirection = 0.0; // -1 => left +1 => right
-        // float rightDistance = ADCRead(disRight);
+        LineSensorReadArray(line, lv);
+        float rightDistance = ADCRead(disRight);
         float leftDistance  = ADCRead(disLeft);
-        float frontDistance = ADCRead(disFront);
+        float power = 0;
         // Drastic change in distance => we need to turn to keep up with wall, set findWall
         // Once we start turning, we need to match the sweetspot but still turn a large amount so we use findWall to ensure this
-        if (avgTriDelta - leftDistance > .3 || (findWall && fabs(leftDistance - sweetSpot) > .1)) {
-            travelDirection = -pow(avgTriDelta, .75); // By raising to a power < 1, we increase the value of a decimal
-            findWall = true;
-        } else if (fabs(avgTriDelta - sweetSpot) > .1) { // Try to get in the sweet spot
-            travelDirection = (avgTriDelta - sweetSpot) * 3;
-            findWall = false;
-        } else if (findWall) {
-            // Stop trying to find the wall when we don't need to anymore
-            findWall = false;
-        }
-        // Inside turn if the front sensor gets close or if we are in progress of inside turning
-        if (frontDistance > .3 || (insideTurn && frontDistance > .2)) {
-            insideTurn = true;
-            travelDirection = 1.0;
-        } else if (insideTurn) {
-            insideTurn = false;
+        if (fabs(avgTriDelta - sweetSpot) > .1) { // Try to get in the sweet spot
+            power = pow(avgTriDelta - sweetSpot, .3);
         }
         // Update avgTriDelta
         avgTriDelta = avgTriDelta * 2.0 / 3.0 + leftDistance * 1.0 / 3.0;
         // Move to the desired travel direction
-        if (travelDirection > 0) {
-            SetSpeed(left, 1.0);
-            SetSpeed(right, pow(1 - travelDirection, 1.8));
-        } else {
-            SetSpeed(left, pow(1 + travelDirection, 1.8));
-            SetSpeed(right, 1.0);
-        }
+        // SetSpeed(left, pow(1 + power, 1.8));
+        // SetSpeed(right, 1.0);
         if (printCount == 500) {
-            Printf("|Left Distance: %.3f|\t|Front Distance: %.3f|\t|", leftDistance, frontDistance);
-            Printf("Avg Tri Delt: %.3f|\t|Travel Direction: %.3f|\n", avgTriDelta, travelDirection);
+            Printf("|Left Distance: %.3f|\t|Right Distance: %.3f|\t|", leftDistance, rightDistance);
+            Printf("Avg Tri Delt: %.3f|\t|Power: %.3f|\n", avgTriDelta, power);
+            Printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", lv[0], lv[1], lv[2], 
+                lv[3], lv[4], lv[5], lv[6], lv[7]); 
             printCount = 0;
         } else {
             printCount++;
